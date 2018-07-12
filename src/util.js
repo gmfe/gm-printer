d4ec5588bff755e139692c8624328b9e42fb4bbe
Chaoming <1173306360@qq.com>
2018-07-12 15:19:48 +0800

    bugfix

--- src/util.js
+++ src/util.js
@@ -1,8 +1,6 @@
-import {version} from '../package.json'
+import { version } from '../package.json'
 
-const __DEBUG__ = process.env.NODE_ENV === 'development'
-
-const printerJS = __DEBUG__ ? '/printer.js' : `//js.guanmai.cn/build/libs/gm-printer/${version}/build/printer.js`; // eslint-disable-line
+const printerJS = `//js.guanmai.cn/build/libs/gm-printer/${version}/build/printer.js` // eslint-disable-line
 
 const fontSizeList = [
   '12px',
