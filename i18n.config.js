6301d8232350a778e8aa51880ec25ebca99606e9
Chaoming <1173306360@qq.com>
2018-12-03 16:34:30 +0800

    大写

--- i18n.config.js
+++ i18n.config.js
@@ -2,7 +2,7 @@ let config = {
   resourceDir: 'resource',
   outputDir: 'out',
   // 下面是双语替换排除的路径
-  exclude: ['demo/*.js', 'src/data_to_key/index.js', 'src/data_to_key/util.js'],
+  exclude: ['demo/*.js', './data_to_key/index.js', './data_to_key/util.js'],
   callStatement: 'i18next.t',
   importStatementStr: 'import {i18next} from \'gm-i18n\';\n'
 }
--- src/data_to_key/util.js
+++ src/data_to_key/util.js
@@ -1,4 +1,3 @@
-import { i18next } from 'gm-i18n'
 import _ from 'lodash'
 import Big from 'big.js'
 
@@ -7,19 +6,19 @@ export const coverDigit2Uppercase = n => {
     return '-'
   }
 
-  const fraction = [i18next.t('角'), i18next.t('分')]
+  const fraction = ['角', '分']
 
   const digit = [
-    i18next.t('零'), i18next.t('壹'), i18next.t('贰'), i18next.t('叁'), i18next.t('肆'),
-    i18next.t('伍'), i18next.t('陆'), i18next.t('柒'), i18next.t('捌'), i18next.t('玖')
+    '零', '壹', '贰', '叁', '肆',
+    '伍', '陆', '柒', '捌', '玖'
   ]
 
   const unit = [
-    [i18next.t('元'), i18next.t('万'), i18next.t('亿')],
-    ['', i18next.t('拾'), i18next.t('佰'), i18next.t('仟')]
+    ['元', '万', '亿'],
+    ['', '拾', '佰', '仟']
   ]
 
-  const head = n < 0 ? i18next.t('欠') : ''
+  const head = n < 0 ? '欠' : ''
 
   n = Math.abs(n)
 
@@ -30,7 +29,7 @@ export const coverDigit2Uppercase = n => {
     right += digit[Math.floor(Big(n).times(Big(10).pow(i + 1)).mod(10).toString())] + fraction[i]
   }
 
-  right = right.replace(/(零.)+$/, '').replace(/(零.)/, i18next.t('零')) || i18next.t('整')
+  right = right.replace(/(零.)+$/, '').replace(/(零.)/, '零') || '整'
 
   n = Math.floor(n)
 
@@ -40,10 +39,10 @@ export const coverDigit2Uppercase = n => {
       p = digit[n % 10] + unit[1][j] + p
       n = Math.floor(n / 10)
     }
-    left = p.replace(/(零.)*零$/, '').replace(/^$/, i18next.t('零')) + unit[0][i] + left
+    left = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + left
   }
 
-  return head + (left.replace(/(零.)*零元/, i18next.t('元')).replace(/(零.)+/g, i18next.t('零')) + right).replace(/^整$/, i18next.t('零元整'))
+  return head + (left.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零') + right).replace(/^整$/, '零元整')
 }
 
 // 12 => S000012  ,  1232131 => S1232131  sid显示,不足6位补足
