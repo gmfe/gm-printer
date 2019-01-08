81dfec8d6797663a2bc5f0b600301d02dc1d43c2
heyong <heyongsoftware@163.com>
2019-01-08 16:06:16 +0800

    分拣核查单模版

--- src/data_to_key/index.js
+++ src/data_to_key/index.js
@@ -1,6 +1,7 @@
 import order from './order'
 import sku from './sku'
 import task from './task'
+import checklist from './checklist'
 
 /**
  * 根据__gm_printer_data_type 处理各种的数据
@@ -15,6 +16,8 @@ export default function toKey (data) {
       return sku(data)
     case 'task':
       return task(data)
+    case 'check_list':
+      return checklist(data)
     default:
       throw new Error(`${data.__gm_printer_data_type} error!`)
   }
